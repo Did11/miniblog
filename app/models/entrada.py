c07890818cc4e96c989ab31ae648e4b6b469d593
Didier Oliva <di.oliva@itecriocuarto.org.ar>
2023-11-11 14:46:10 -0300

    fix models

--- app/models/entrada.py
+++ app/models/entrada.py
@@ -4,7 +4,8 @@ class Entrada(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     titulo = db.Column(db.String(140))
     cuerpo = db.Column(db.String(1000))
-    usuario_id = db.Column(db.Integer, db.ForeignKey('usuario.id'))
+    usuario_id = db.Column(db.Integer, db.ForeignKey('usuario.id', name='fk_usuario_id'))
+    categoria_id = db.Column(db.Integer, db.ForeignKey('categoria.id', name='fk_categoria_id'))  # Nombre asignado
 
     def __repr__(self):
         return f'<Entrada {self.titulo}>'
